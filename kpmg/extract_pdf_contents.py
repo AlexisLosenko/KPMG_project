2be65d4d9e4b94be0505c51e0b4455e76434ee76
niels-s <stevens.niels@gmail.com>
2019-09-11 11:36:44 +0200

    OCR update

--- kpmg/extract_pdf_contents.py
+++ kpmg/extract_pdf_contents.py
@@ -1,9 +1,12 @@
 import os
-import textract
 import spacy
 from pymongo.errors import DuplicateKeyError
 from spacy_langdetect import LanguageDetector
 from pymongo import MongoClient
+from wand.image import Image as WandImg
+from PIL import Image
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
 
 nlp = spacy.load("en_core_web_sm")
 nlp.add_pipe(LanguageDetector(), name="language_detector", last=True)
@@ -16,9 +19,11 @@ def pdfsToTxt(folderPath):
             if ".pdf" in file:
                 files.append(os.path.join(r, file))
     for f in files:
-        pdf_extract = textract.process(f)
-        doc = nlp(pdf_extract.decode("utf8"))
-        pdf_text = encode_newlines(str(pdf_extract))
+        pdf_img = Image.open(convert_to_img(f, 300))
+        pdf_string = pytesseract.image_to_string(pdf_img)
+        doc = nlp(pdf_string)
+
+        pdf_text = encode_newlines(pdf_string)
 
         language = doc._.language["language"]
         txt_filename = format_txt_filename(f, language)
@@ -41,7 +46,6 @@ def pdfsToTxt(folderPath):
             print("Id already in database: " + str(e))
 
 
-
 def encode_newlines(text) :
     return text.replace("\\r\\n", "\r\n").replace("\\n", "\n").replace("\\r", "\r")
 
@@ -52,6 +56,20 @@ def format_txt_filename(original_pdf, language) :
     return os.path.abspath(original_pdf).replace(".pdf", "_").replace("\\pdf", "\\txt") + \
     language + \
     ".txt"
+
+def convert_to_img(file, resolution) :
+    file_path = os.path.abspath(file)
+    img_path = file_path.replace("pdf", "png")
+
+    if os.path.isfile(img_path):
+        print("Image already exists: " + img_path)
+    else :
+        with WandImg(filename=file_path, resolution=resolution) as img:
+            img.compression_quality = 99
+            img.save(filename=img_path)
+            print("Image created: " + img_path)
+
+    return img_path
 
 
 pdfsToTxt(".\\pdf")
