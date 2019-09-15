f55f010bf2e4f147ca5b8e8e49e60cb639986a2f
sebchko <sebastien.pardon@gmail.com>
2019-09-16 01:05:22 +0200

    not sure is better :) but : rework on the dictionnaries and the meta_dic gen

--- meta_gen/meta_gen.py
+++ meta_gen/meta_gen.py
@@ -1,75 +1,97 @@
 import pandas as pd
-def dict_gen(element,activity,table02,table04,table05,table06,table07,trad01_jurid_sit, trad02_type, trad03_jur_form, trad04_nacebel):
-
-    table07group = table07.groupby('EnterpriseNumber').count()
-#            table07group.StartDate[table07group.index == element]
+def dict_gen(element,activity,table02,table04,table05,table06,table07,
+             ActivityGroup_dic,  JuridicalForm_dic, JuridicalSituation_dic, Nace2003_dic, Nace2008_dic,  TypeOfEnterprise_dic):
 
     meta_dic = dict()
 
-    table06_row = table06[table06['EnterpriseNumber']== element]
-    #check if row is empty -> skip -> otherwise :
-    meta_dic['VAT Number']=  table06_row['EnterpriseNumber']
-    meta_dic["other"] = table06_row["other"]
+    table06_row = table06[table06['EnterpriseNumber'] == element]
+    table05_row = table05[table05['EntityNumber'] == element]
+    table05_row_abbr = table05[(table05['EntityNumber'] == element) & (table05.TypeOfDenomination == 2)]
+    table01_row_main = activity[(activity['EntityNumber'] == element) & (activity.Classification == 'MAIN')].astype(str)
+    table01_row_sec = activity[(activity['EntityNumber'] == element) & (activity.Classification == 'SECO')]
+    table07_row = table07[table07['EnterpriseNumber'] == element]
+    table02_row = table02[table02['EntityNumber'] == element]
+    table04_row_mail = table04[(table04['EntityNumber'] == element) & (table04['ContactType'] == 'EMAIL')]
+    table04_row_phone = table04[(table04['EntityNumber'] == element) & (table04['ContactType'] == 'TEL')]
 
-    meta_dic['VAT Number']=  table06['EnterpriseNumber'][table06['EnterpriseNumber']== element].tolist()[0] 
+    if len(table06_row) == 0:
+        print('not in the csv')
+    else:
+        meta_dic['VAT Number'] = table06_row.iloc[0]['EnterpriseNumber']
 
-    meta_dic['Denomination'] = table05['Denomination'][(table05['EntityNumber'] == element)].tolist()[0]
+        meta_dic['Denomination'] = table05_row.iloc[0]['Denomination']
 
-#meta_dic['Abbr'] = table05['Denomination'][(table05['EntityNumber'] == element)].tolist()[1]
+        meta_dic['Abbr'] = (lambda x: table05_row_abbr.iloc[0]['Denomination']
+        if len(table05_row_abbr) != 0 else 'NONE')(element)
 
-    meta_dic['Abbr'] = (lambda x: table05['Denomination'][(table05['EntityNumber'] == x)&(table05.TypeOfDenomination == 2)].tolist() 
-        if len(table05['Denomination'][(table05['EntityNumber'] == x)&(table05.TypeOfDenomination == 2)].tolist()) != 0 
-        else 'NONE')(element)
+        meta_dic['ActivityGroup'] = (lambda x: table01_row_main.iloc[0]['ActivityGroup']
+        if len(table01_row_main) != 0 else "none")(element)
 
-    meta_dic['Main activity'] = activity['NaceCode'][(activity.EntityNumber == element)&(activity.Classification == 'MAIN')].to_list()
+        meta_dic['Main activity'] = (
+            lambda x: [table01_row_main.iloc[0]['NaceVersion'], table01_row_main['NaceCode'].unique().tolist()]
+            if len(table01_row_main) != 0 else "none")(element)
 
-    meta_dic['Secondary activity'] = (lambda x: activity['NaceCode'][(activity.EntityNumber == x)&(activity.Classification == 'SECO')].tolist() 
-        if len(activity['NaceCode'][(activity.EntityNumber == x)&(activity.Classification == 'SECO')].tolist()) != 0 else 'NONE')(element)
+        meta_dic['Secondary activity'] = (lambda x: table01_row_sec['NaceCode'].unique().tolist()
+        if len(table01_row_sec) != 0 else "none")(element)
 
-    meta_dic['Foundation Date']=table06['StartDate'][table06['EnterpriseNumber']== element].tolist()[0]
+        meta_dic['Foundation Date'] = table06_row['StartDate'].iloc[0]
 
-    meta_dic['Establishment #'] = (lambda x: int(table07group.StartDate[table07group.index == x]) 
-        if len(pd.Series(table07group.index == x).unique())== 2  else 0)(element)
+        meta_dic['Establishment #'] = len(table07_row)
 
-    meta_dic['Establishment StartDate'] = table07['StartDate'][table07['EnterpriseNumber']==element].to_list()
+        meta_dic['Establishment StartDate'] = table07_row['StartDate'].to_list()
 
-    meta_dic['JuridicalSituation']= int(table06['JuridicalSituation'][table06['EnterpriseNumber']==element])
+        meta_dic['JuridicalSituation'] = str(table06_row.iloc[0]['JuridicalSituation'])
 
-    meta_dic['TypeOfEnterprise']=int(table06['TypeOfEnterprise'][table06['EnterpriseNumber']==element])
+        meta_dic['TypeOfEnterprise'] = str(table06_row.iloc[0]['TypeOfEnterprise'])
 
-    meta_dic['JuridicalForm']=int(table06['JuridicalForm'][table06['EnterpriseNumber']==element])
+        meta_dic['JuridicalForm'] = str(table06_row.iloc[0]['JuridicalForm'])
 
-#meta_dic['Language']=int(table06['TypeOfEnterprise'][table06['EnterpriseNumber']==EN[0]])
+        # meta_dic['Language']= table06_row.iloc[0]['TypeOfEnterprise']
 
-    meta_dic['Zipcode'] = int(table02['Zipcode'][table02['EntityNumber'] == element])
+        meta_dic['Zipcode'] = table02_row.iloc[0]['Zipcode']
 
-    meta_dic['Street'] = table02['StreetFR'][table02['EntityNumber'] == element].tolist()[0]
+        meta_dic['Street'] = table02_row.iloc[0]['StreetFR']
 
-    meta_dic['HouseNumber'] = table02['HouseNumber'][table02['EntityNumber'] == element].astype(str).tolist()[0]
+        meta_dic['HouseNumber'] = table02_row.iloc[0]['HouseNumber']
 
-    meta_dic['Mail'] = (lambda x: table04['Value'][(table04['EntityNumber']==x) &(table04['ContactType']== 'EMAIL')].tolist()[0] 
-        if len(table04['Value'][(table04['EntityNumber'] == x)&(table04['ContactType']== 'EMAIL')].tolist()) != 0  else 'NA')(element)
+        meta_dic['Mail'] = (lambda x: table04_row_mail.iloc[0]['Value']
+        if len(table04_row_mail) != 0 else 'NA')(element)
 
-    meta_dic['Phone'] = (lambda x: table04['Value'][(table04['EntityNumber']==x) &(table04['ContactType']== 'TEL')].tolist()[0] 
-        if len(table04['Value'][(table04['EntityNumber'] == x)&(table04['ContactType']== 'TEL')].tolist()) != 0  else 'NA')(element)
+        meta_dic['Phone'] = (lambda x: table04_row_phone.iloc[0]['Value']
+        if len(table04_row_phone) != 0 else 'NA')(element)
 
+        for KEY, VALUE in ActivityGroup_dic.items():
+            if meta_dic['ActivityGroup'] == KEY:
+                meta_dic['ActivityGroup'] = VALUE
 
-    for key, value in meta_dic.items():
-        for KEY, VALUE in trad01_jurid_sit.items():
-            if meta_dic['JuridicalSituation'] == KEY:
-                meta_dic['JuridicalSituation'] = VALUE
-        for KEY, VALUE in trad02_type.items():
-            if meta_dic['TypeOfEnterprise'] == KEY:
-                meta_dic['TypeOfEnterprise'] = VALUE            
-        for KEY, VALUE in trad03_jur_form.items():
+        for KEY, VALUE in JuridicalForm_dic.items():
             if meta_dic['JuridicalForm'] == KEY:
                 meta_dic['JuridicalForm'] = VALUE
-        for KEY, VALUE in trad04_nacebel.items():
-            for i in range (0,len(meta_dic['Main activity'])):
-                if meta_dic['Main activity'][i] == KEY:
-                    meta_dic['Main activity'][i] = (KEY,VALUE)
-        for KEY, VALUE in trad04_nacebel.items():
-            for i in range (0,len(meta_dic['Secondary activity'])):
+
+        for KEY, VALUE in JuridicalSituation_dic.items():
+            if meta_dic['JuridicalSituation'] == KEY:
+                meta_dic['JuridicalSituation'] = VALUE
+
+        for KEY, VALUE in TypeOfEnterprise_dic.items():
+            if meta_dic['TypeOfEnterprise'] == KEY:
+                meta_dic['TypeOfEnterprise'] = VALUE
+
+        if meta_dic['Main activity'][0] == 2003:
+            for KEY, VALUE in Nace2003_dic.items():
+                for i in range(0, len(meta_dic['Main activity'])):
+                    if meta_dic['Main activity'][1][i] == KEY:
+                        meta_dic['Main activity'][1][i] = (KEY, VALUE)
+
+            for i in range(0, len(meta_dic['Secondary activity'])):
                 if meta_dic['Secondary activity'][i] == KEY:
-                    meta_dic['Secondary activity'][i] = (KEY,VALUE)
+                    meta_dic['Secondary activity'][i] = (KEY, VALUE)
+        else:
+            for KEY, VALUE in Nace2008_dic.items():
+                for i in range(0, len(meta_dic['Main activity'][1])):
+                    if meta_dic['Main activity'][1][i] == KEY:
+                        meta_dic['Main activity'][1][i] = (KEY, VALUE)
+
+            for i in range(0, len(meta_dic['Secondary activity'])):
+                if meta_dic['Secondary activity'][i] == KEY:
+                    meta_dic['Secondary activity'][i] = (KEY, VALUE)
     return(meta_dic)
--- meta_gen/update_db_with_meta.py
+++ meta_gen/update_db_with_meta.py
@@ -13,6 +13,13 @@ table04 = pd.read_csv('./OpenData/contact.csv')
 table05 = pd.read_csv('./OpenData/denomination.csv')
 table06 = pd.read_csv('./OpenData/enterprise.csv')
 table07 = pd.read_csv('./OpenData/establishment.csv')
+#needed for later...
+temp = table06.JuridicalForm.fillna(0).astype(int).astype(str)
+temp2 = table06.JuridicalSituation.fillna(0).astype(int).astype(str)
+temp = ["0"+i if len(i)==2 else "00"+i if len(i)==1 else i for i in temp]
+temp2 = ["0"+i if len(i)==2 else "00"+i if len(i)==1 else i for i in temp2]
+table06.JuridicalForm = temp
+table06.JuridicalSituation = temp2
 
 print('tables loaded')
 #need probably to be re-scraped to match with the pdf's
@@ -23,8 +30,8 @@ vat_numbers = vat_numbers[:10]
 
 vat_numbers = "0" + vat_numbers.astype(str)
 #formatting TVA number
-vat_numbers[1]= vat_numbers[0].str[:3] + '.' + vat_numbers[0].str[3:6] + \
-                '.'+vat_numbers[0].str[6:9]
+vat_numbers[1]= vat_numbers[0].str[:4] + '.' + vat_numbers[0].str[4:7] + \
+                '.'+vat_numbers[0].str[7:10]
 vat_uids = vat_numbers[0].tolist()
 vat_formatted = vat_numbers[1].tolist()
 
@@ -33,7 +40,7 @@ print(vat_formatted)
 
 
 print('loading dictionaries')
-trad01_jurid_sit,  trad02_type, trad03_jur_form,trad04_nacebel = load_dict()
+ActivityGroup_dic,  JuridicalForm_dic, JuridicalSituation_dic, Nace2003_dic, Nace2008_dic,TypeOfEnterprise_dic = load_dict(table03)
 
 
 def update_statutes_in_db(uids):
@@ -43,8 +50,8 @@ def update_statutes_in_db(uids):
     for uid in uids :
         stat_doc = stat_coll.find_one({"_id": uid})
         if stat_doc :
-            #meta_dic = dict_gen(uid, activity, table02, table04, table05, table06, table07, trad01_jurid_sit,
-            #                    trad02_type, trad03_jur_form, trad04_nacebel)
+            #meta_dic = dict_gen(uid, activity, table02, table04, table05, table06, table07,
+            # ActivityGroup_dic,  JuridicalForm_dic, JuridicalSituation_dic, Nace2003_dic, Nace2008_dic, TypeOfEnterprise_dic )
             #TODO uncomment above and remove below
             meta_dic = {    "Formatted VAT Number": "0686.796.523",
                             "Denomination": "A&M RENOVA CONSTRUCT",
