1e5ecde95661470b7d8ac9d758ecde55d537c5c4
sebchko <sebastien.pardon@gmail.com>
2019-09-17 12:25:25 +0200

    formatting uid

--- meta_gen/meta_gen.py
+++ meta_gen/meta_gen.py
@@ -1,10 +1,13 @@
 import pandas as pd
+from scrap_act_object import act_obj
 def dict_gen(uid, activity, table02, table04, table05, table06, table07,
              ActivityGroup_dic, JuridicalForm_dic, JuridicalSituation_dic, Nace2003_dic, Nace2008_dic, TypeOfEnterprise_dic):
 
     meta_dic = dict()
-
+    uid_f = str(uid[:4]+'.'+str(uid[4:7])+'.'+str(uid[7:10]))
+    print(uid_f)
     # formatting 2 features from table 06 to match with the code table (table03)
+
     temp = table06.JuridicalForm.fillna(0).astype(int).astype(str)
     temp2 = table06.JuridicalSituation.fillna(0).astype(int).astype(str)
     temp = ["0" + i if len(i) == 2 else "00" + i if len(i) == 1 else i for i in temp]
@@ -12,35 +15,37 @@ def dict_gen(uid, activity, table02, table04, table05, table06, table07,
     table06.JuridicalForm = temp
     table06.JuridicalSituation = temp2
 
-    table06_row = table06[table06['EnterpriseNumber'] == uid]
-    table05_row = table05[table05['EntityNumber'] == uid]
-    table05_row_abbr = table05[(table05['EntityNumber'] == uid) & (table05.TypeOfDenomination == 2)]
-    table01_row_main = activity[(activity['EntityNumber'] == uid) & (activity.Classification == 'MAIN')].astype(str)
-    table01_row_sec = activity[(activity['EntityNumber'] == uid) & (activity.Classification == 'SECO')]
-    table07_row = table07[table07['EnterpriseNumber'] == uid]
-    table02_row = table02[table02['EntityNumber'] == uid]
-    table04_row_mail = table04[(table04['EntityNumber'] == uid) & (table04['ContactType'] == 'EMAIL')]
-    table04_row_phone = table04[(table04['EntityNumber'] == uid) & (table04['ContactType'] == 'TEL')]
+    table06_row = table06[table06['EnterpriseNumber'] == uid_f]
+    table05_row = table05[table05['EntityNumber'] == uid_f]
+    table05_row_abbr = table05[(table05['EntityNumber'] == uid_f) & (table05.TypeOfDenomination == 2)]
+    table01_row_main = activity[(activity['EntityNumber'] == uid_f) & (activity.Classification == 'MAIN')].astype(str)
+    table01_row_sec = activity[(activity['EntityNumber'] == uid_f) & (activity.Classification == 'SECO')]
+    table07_row = table07[table07['EnterpriseNumber'] == uid_f]
+    table02_row = table02[table02['EntityNumber'] == uid_f]
+    table04_row_mail = table04[(table04['EntityNumber'] == uid_f) & (table04['ContactType'] == 'EMAIL')]
+    table04_row_phone = table04[(table04['EntityNumber'] == uid_f) & (table04['ContactType'] == 'TEL')]
 
     if len(table06_row) == 0:
         print("No data found for vat number " + uid + " in the CSVs")
     else:
         meta_dic['VAT Number'] = table06_row.iloc[0]['EnterpriseNumber']
 
+        #meta_dic['act object'] = act_obj(uid)
+
         meta_dic['Denomination'] = table05_row.iloc[0]['Denomination']
 
         meta_dic['Abbr'] = (lambda x: table05_row_abbr.iloc[0]['Denomination']
-        if len(table05_row_abbr) != 0 else 'NONE')(uid)
+        if len(table05_row_abbr) != 0 else 'NONE')(uid_f)
 
         meta_dic['ActivityGroup'] = (lambda x: table01_row_main.iloc[0]['ActivityGroup']
-        if len(table01_row_main) != 0 else "none")(uid)
+        if len(table01_row_main) != 0 else "none")(uid_f)
 
         meta_dic['Main activity'] = (
             lambda x: [table01_row_main.iloc[0]['NaceVersion'], table01_row_main['NaceCode'].unique().tolist()]
-            if len(table01_row_main) != 0 else "none")(uid)
+            if len(table01_row_main) != 0 else "none")(uid_f)
 
         meta_dic['Secondary activity'] = (lambda x: table01_row_sec['NaceCode'].unique().tolist()
-        if len(table01_row_sec) != 0 else "none")(uid)
+        if len(table01_row_sec) != 0 else "none")(uid_f)
 
         meta_dic['Foundation Date'] = table06_row['StartDate'].iloc[0]
 
@@ -63,10 +68,10 @@ def dict_gen(uid, activity, table02, table04, table05, table06, table07,
         meta_dic['HouseNumber'] = table02_row.iloc[0]['HouseNumber']
 
         meta_dic['Mail'] = (lambda x: table04_row_mail.iloc[0]['Value']
-        if len(table04_row_mail) != 0 else 'NA')(uid)
+        if len(table04_row_mail) != 0 else 'NA')(uid_f)
 
         meta_dic['Phone'] = (lambda x: table04_row_phone.iloc[0]['Value']
-        if len(table04_row_phone) != 0 else 'NA')(uid)
+        if len(table04_row_phone) != 0 else 'NA')(uid_f)
 
         for KEY, VALUE in ActivityGroup_dic.items():
             if meta_dic['ActivityGroup'] == KEY:
@@ -102,4 +107,7 @@ def dict_gen(uid, activity, table02, table04, table05, table06, table07,
             for i in range(0, len(meta_dic['Secondary activity'])):
                 if meta_dic['Secondary activity'][i] == KEY:
                     meta_dic['Secondary activity'][i] = (KEY, VALUE)
+
+        print(meta_dic)
+
     return(meta_dic)
