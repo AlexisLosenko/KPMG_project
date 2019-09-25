5e6588a4dde39fb17c2f62568e36b09bd3d9fac2
Marco <mcg.tampieri@gmail.com>
2019-09-25 17:02:23 +0200

    Progress on front

--- pymongoexample/__init__.py
+++ pymongoexample/__init__.py
@@ -1,3 +1,4 @@
+import re
 from flask import Flask, render_template, request
 from flask_pymongo import PyMongo
 
@@ -13,21 +14,65 @@ mongo = PyMongo(app)
 @app.route('/', methods=['GET','POST'])
 def home():
     print('we re here')
+
     if request.method == 'POST':
-        check = request.form['numberTVA']
-        print('working? ::', check)
-        return findOne(check)
+        if request.form['numberTVA'] != '' and request.form['compName'] == '' and request.form['creatDate'] == '':
+            check = request.form['numberTVA']
+            print('id is: ', check)
+            return findById(check)
+        elif request.form['compName'] != '' and request.form['numberTVA'] == '' and request.form['creatDate'] == '':
+            name = request.form['compName']
+            print('name is: ', name)
+            return findByName(name)
+        elif request.form['creatDate'] != '' and request.form['numberTVA'] == '' and request.form['compName'] == '':
+            date = request.form['creatDate']
+            print('creation date: ', date)
+            return findByDate(date)
+        else:
+            print('nothing to look for')
+            return 'nothing to look for'
+
+        
+
 
     return render_template("test.html")
 
 
-@app.route('/findOne')
-def findOne(y):
-    try:
-        company = mongo.db.statutes.find({"_id": y})
-        return render_template('test.html', company = company)
-    except Exception as e:
-        return e 
+@app.route('/findById')
+def findById(y):
+    
+    company = mongo.db.statutes.find({"_id": y})
+    return render_template('test.html', company = company)
+
+@app.route('/findByName')
+def findByName(y):
+    cleanString = re.sub('\W+',' ', y)  # r in front of '\W+'
+    stringList = cleanString.split()
+    print(stringList)
+    pattern_string = ''
+    for word in stringList:
+        pattern_string += f'(?:{word})|'
+    pattern_string = pattern_string[:-1]
+    # company = mongo.db.statutes.find({"Denomination": {"$regex" : pattern_string}})
+    company = mongo.db.statutes.find({"Denomination": re.compile(pattern_string, re.IGNORECASE)})
+    #company = mongo.db.statutes.find({"Denomination": y})
+    return render_template('test.html', company = company)
+
+
+
+@app.route('/findByDate')
+def findByDate(y):
+    company = []
+    for doc in mongo.db.statutes.find():
+        if y in doc['documents'].keys():
+            print('deep check')
+            #company = doc['documents'][y]['text']
+            #print(company)
+            company.append(doc)
+    # textDir = mongo.db.statutes.find({f"documents.{y}"})
+    # company = textDir.text
+
+    return render_template('test.html', company = company, date = y)
 
 # def create_app(config_object='pymongoexample.settings'):
 #     app = Flask(__name__)
